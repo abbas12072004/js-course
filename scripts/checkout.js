eb6c9ac46e8ee0837a38f3ba1530fd52b98d3c09
abbas <abbas@gmaiil.com>
2026-05-07 03:00:21 +0530

    halfway through adding delete funtion

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -51,7 +51,8 @@ cartSummaryHTML+=`
           <span class="update-quantity-link link-primary">
             Update
           </span>
-          <span class="delete-quantity-link link-primary">
+          <span class="delete-quantity-link link-primary js-delete-link"
+          data-product-id="${matchingProduct.id}">
             Delete
           </span>
         </div>
@@ -109,5 +110,13 @@ cartSummaryHTML+=`
 });
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+
+
+document.querySelectorAll('.js-delete-link').forEach((link) => {
+  link.addEventListener('click',() => {
+    const productId = link.dataset.productId;
+    console.log(productId);
+  });
+});
 
 
