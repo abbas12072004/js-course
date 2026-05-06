754d74686c991a62360e40379f245da52c554700
abbas <abbas@gmaiil.com>
2026-05-06 18:14:35 +0530

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -63,34 +63,41 @@ document.querySelector('.js-product-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) =>
 {
- button.addEventListener('click',() => {
-  const productId= button.dataset.productId;
+  button.addEventListener('click',() => {
+    const productId= button.dataset.productId;
 
 
 
-  let matchingItem;
+    let matchingItem;
 
-  cart.forEach((item) => {
-    if (productId === item.productId) {
-      matchingItem = item;
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1
+      });
     }
+
+    let cartQuantity = 0;
+      cart.forEach((item) => {
+    cartQuantity += item.quantity;
+      });
+    
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+    
   });
 
-  if (matchingItem) {
-    matchingItem.quantity += 1;
-  } else {
-    cart.push({
-      productId: productId,
-      quantity: 1
-  });
-  }
-
-
-
-
-  console.log(cart);
- });
 });
+
+
 
 
 
