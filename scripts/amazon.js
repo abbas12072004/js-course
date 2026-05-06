2427e20c190c60e69f0b3e2549d595d34b6836ad
abbas <abbas@gmaiil.com>
2026-05-06 17:57:16 +0530

    made the cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -48,7 +48,8 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+          data-product-name="${product.name}">
             Add to Cart
           </button>
         </div>
@@ -58,6 +59,38 @@ products.forEach((product) => {
 
 document.querySelector('.js-product-grid').innerHTML = productsHTML;
 
+
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) =>
+{
+ button.addEventListener('click',() => {
+  const productName= button.dataset.productName;
+
+
+
+  let matchingItem;
+
+  cart.forEach((item) => {
+    if (productName === item.productName) {
+      matchingItem = item;
+    }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productName: productName,
+      quantity: 1
+  });
+  }
+
+
+
+
+  console.log(cart);
+ });
+});
 
 
 
